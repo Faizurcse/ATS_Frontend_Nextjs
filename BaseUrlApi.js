c29ba67862c658390cbb5c7386e8f41a24543999
Faizurcse <mdfaizurrahman2000@gmail.com>
2025-08-13 15:13:35 +0530

    upadted env

--- BaseUrlApi.js
+++ BaseUrlApi.js
@@ -1,8 +1,8 @@
 // Centralized API base URL
 // For local development
-const BASE_API_URL = "http://localhost:5000/api"
+// const BASE_API_URL = "http://localhost:5000/api"
 
 // For production (uncomment when deploying)
-// const BASE_API_URL = "https://ats-backend-nodejs-h80i.onrender.com/api"
+const BASE_API_URL = "https://ats-backend-nodejs-h80i.onrender.com/api"
 
 export default BASE_API_URL; 
