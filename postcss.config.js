713b762e733450a32fa96de632c33b5403dc8cd0
Faizurcse <mdfaizurrahman2000@gmail.com>
2025-09-02 22:43:18 +0530

    Fix: Re-enable PostCSS plugins for TailwindCSS support on live server

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    // tailwindcss: {}, // Temporarily disabled for VPS build
-    // autoprefixer: {}, // Temporarily disabled for VPS build
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
