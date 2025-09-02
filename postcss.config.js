85675b62c9fdd8637229ebf0222936837e23a705
Faizurcse <mdfaizurrahman2000@gmail.com>
2025-09-02 23:03:45 +0530

    Fix: Disable PostCSS plugins for VPS build compatibility

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    // tailwindcss: {}, // Disabled for VPS build
+    // autoprefixer: {}, // Disabled for VPS build
   },
 };
